666e52c0d5a431d078598c91cf29d3eab34445a5
Seokkitdo <sjd12329@gmail.com>
2021-08-17 16:29:32 +0900

    refactor : 정렬

--- src/App.js
+++ src/App.js
@@ -4,34 +4,31 @@ import DateTime from "components/DateTime";
 import "styles/app.scss";
 
 function App() {
-    const inputNums = useRef(null);
-    const [arrData, setArrData] = useState("");
+  const inputNums = useRef(null);
+  const [arrData, setArrData] = useState("");
 
-    // input 에서 값 가져옴
-    function getValueFromInput() {
-        const inputValue = inputNums.current.value;
-        return inputValue;
-    }
+  // input 에서 값 가져옴
+  function getValueFromInput() {
+    const inputValue = inputNums.current.value;
+    return inputValue;
+  }
 
-    // 버튼 클릭 시
-    function handleOnClick() {
-        const value = getValueFromInput();
-        setArrData(value);
-    }
+  // 버튼 클릭 시
+  function handleOnClick() {
+    const value = getValueFromInput();
+    setArrData(value);
+  }
 
-    return (
-        <div className="wrapper">
-            <DateTime type="kr" />
-            <input
-                type="text"
-                ref={inputNums}
-                placeholder="숫자를 입력해주세요"
-            />
-            <button onClick={handleOnClick}>SORT</button>
-            <Result inputValue={arrData} />
-            <DateTime type="us" />
-        </div>
-    );
+  return (
+    <div className="wrapper">
+      <DateTime type="kr" />
+      <input type="text" ref={inputNums} placeholder="숫자를 입력해주세요" />
+      <button onClick={handleOnClick}>SORT</button>
+      <Result inputValue={arrData} desc={false} />
+      <Result inputValue={arrData} desc={true} />
+      <DateTime type="us" />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Result.js
+++ src/components/Result.js
@@ -1,93 +1,63 @@
 import React, { useState, useEffect } from "react";
 
-function Result({ inputValue }) {
-    const [arrAsc, setArrAsc] = useState([]);
-    const [arrDesc, setArrDesc] = useState([]);
-    const [waiting, setWaiting] = useState(true);
+function Result({ inputValue, desc }) {
+  const [arrAsc, setArrAsc] = useState([]);
+  const [arrDesc, setArrDesc] = useState([]);
+  const [waiting, setWaiting] = useState(true);
 
-    useEffect(() => {
-        init();
-        setWaiting(true);
-    }, [inputValue]);
+  useEffect(() => {
+    init();
+    setWaiting(true);
+  }, [inputValue]);
 
-    // 배열을 내림차순으로 정렬
-    const sortDesc = (value) => {
-        let arr = value.slice();
-        for (let i = 0; i < arr.length; i++) {
-            for (let j = i; j > 0; j--) {
-                //크면 교환
-                if (arr[j] > arr[j - 1]) {
-                    let tmp = arr[j];
-                    arr[j] = arr[j - 1];
-                    arr[j - 1] = tmp;
-                } else {
-                    break;
-                }
-            }
+  const sort = (value, desc) => {
+    let arr = value.slice();
+
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = i; j > 0; j--) {
+        if (desc ? arr[j] > arr[j - 1] : arr[j] < arr[j - 1]) {
+          let tmp = arr[j];
+          arr[j] = arr[j - 1];
+          arr[j - 1] = tmp;
+        } else {
+          break;
         }
-        setArrDesc(arr);
-    };
-
-    // 배열을 오름차순으로 정렬
-    function sortAsc(value) {
-        let arr = value.slice();
-        for (let i = 0; i < arr.length; i++) {
-            for (let j = i; j > 0; j--) {
-                if (arr[j] < arr[j - 1]) {
-                    let tmp = arr[j];
-                    arr[j] = arr[j - 1];
-                    arr[j - 1] = tmp;
-                } else {
-                    break;
-                }
-            }
-        }
-        setArrAsc(arr);
+      }
     }
+    desc ? setArrDesc(arr) : setArrAsc(arr);
+  };
 
-    // 가져온 값을 배열로 변경
-    function makeArrayfromString(value) {
-        const arr = value.split(",").map((item) => {
-            return parseInt(item, 10);
-        });
-        return arr;
-    }
+  // 가져온 값을 배열로 변경
+  function makeArrayfromString(value) {
+    const arr = value.split(",").map((item) => {
+      return parseInt(item, 10);
+    });
+    return arr;
+  }
 
-    function init() {
-        const arr = makeArrayfromString(inputValue);
-        sortAsc(arr);
-        setTimeout(() => {
-            sortDesc(arr);
-            setWaiting(false);
-        }, 3000);
-    }
+  function init() {
+    const arr = makeArrayfromString(inputValue);
+    sort(arr, false);
+    setTimeout(() => {
+      sort(arr, true);
+      setWaiting(false);
+    }, 3000);
+  }
 
-    return (
-        <div className="wrapper__result">
-            <div className="result">
-                <div className="result__title">오름차순</div>
-                <div className="result__content">
-                    {arrAsc.map((item, index) => (
-                        <span key={index}>
-                            {index !== 0 ? ", " + item : item}
-                        </span>
-                    ))}
-                </div>
-            </div>
-            <div className="result">
-                <div className="result__title">내림차순 </div>
-                <div className="result__content">
-                    {waiting
-                        ? "waiting"
-                        : arrDesc.map((item, index) => (
-                              <span key={index}>
-                                  {index !== 0 ? ", " + item : item}
-                              </span>
-                          ))}
-                </div>
-            </div>
+  return (
+    <div className="wrapper__result">
+      <div className="result">
+        <div className="result__title">{desc ? "내림차순" : "오름차순"}</div>
+        <div className="result__content">
+          {desc ? <>{waiting ? "waiting" : <span>{arrDesc.join(", ")}</span>}</> : <span>{arrAsc.join(", ")}</span>}
         </div>
-    );
+      </div>
+      {/* <div className="result">
+        <div className="result__title">내림차순 </div>
+        <div className="result__content">{waiting ? "waiting" : <span>{arrDesc.join(", ")}</span>}</div>
+      </div> */}
+    </div>
+  );
 }
 
 export default Result;
